92a878f27bcccf11e8cdfeb00809df322ee6e94f
Gilad Teller <Gilad.Teller@toluna.com>
2023-02-16 10:18:11 +0200

    refactor code and add git diff fallback

--- GitGpt/GitGpt.py
+++ GitGpt/GitGpt.py
@@ -2,24 +2,34 @@ import os
 import openai
 import subprocess
 
-api_key = os.getenv("OPENAI_API_KEY")
-if not api_key:
-    raise Exception('No OPENAI_API_KEY environment variable')
+def run_subprocess(command):
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.stderr:
+        raise Exception('{} returned error'.format(command), result.stderr)
+    return result.stdout
 
-organization = os.getenv("OPENAI_ORG")
-if not api_key:
-    raise Exception('No OPENAI_ORG environment variable')
+def get_env_var(name):
+    env_var = os.getenv(name)
+    if not env_var:
+        raise Exception('No {} environment variable'.format(name))
+    return env_var
 
+print('Getting environment variables')
+api_key = get_env_var("OPENAI_API_KEY")
+organization = get_env_var("OPENAI_ORG")
 
-result = subprocess.run('git diff --cached', capture_output=True, text=True)
-if result.stderr:
-    raise Exception('git diff --cached returned error', result.stderr)
-if not result.stdout:
-    raise Exception('git diff --cached returned no changes', result.stderr)
+print('Running git diff --cached')
+result = run_subprocess('git diff --cached')
+if not result:
+    result = run_subprocess('git diff')
+if not result:
+    print('No result, running git diff')
+    raise Exception('No diff')
 
+print('Calling OpenAI')
 openai.organization = organization
 openai.api_key = api_key
-text = 'Suggest me a few good commit messages for my commit following conventional commit (<type>: <subject>). \n' + result.stdout
+text = 'Suggest me a few good commit messages for my commit following conventional commit (<type>: <subject>). \n' + result
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt=text,
