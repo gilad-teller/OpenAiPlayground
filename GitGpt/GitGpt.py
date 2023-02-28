f4a7bbc2e0bd2216b00273366669e7eea388ee85
Gilad Teller <Gilad.Teller@toluna.com>
2023-02-28 16:39:16 +0200

    fix: Refactor GitGpt.py to use prompt instead of text

--- GitGpt/GitGpt.py
+++ GitGpt/GitGpt.py
@@ -34,10 +34,10 @@ if not result:
 print('Calling OpenAI')
 openai.organization = organization
 openai.api_key = api_key
-text = 'Suggest me a few good commit messages for my commit following conventional commit (<type>: <subject>). Return all suggestions as json array. \n' + result
+prompt = 'Suggest me a few good commit messages for my commit following conventional commit (<type>: <subject>). Return all suggestions as json array. \n' + result
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=text,
+  prompt=prompt,
   temperature=0.7,
   max_tokens=100,
   top_p=1,
